37f739cb7cf92b174fdfd719e7a5386237d06105
firstbloodidp <sergiu_andrei96@yahoo.com>
2018-03-22 17:53:39 +0200

    Index of the work platform page

--- CallCenter/App_Start/BundleConfig.cs
+++ CallCenter/App_Start/BundleConfig.cs
@@ -24,8 +24,8 @@ namespace CallCenter
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap-darky.css",
+                      "~/Content/MyCSS.css"));
         }
     }
 }
