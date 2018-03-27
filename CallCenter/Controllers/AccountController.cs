441ba4914e72977713175abfd5dbb20371559613
firstbloodidp <sergiu_andrei96@yahoo.com>
2018-03-27 19:56:16 +0300

    Change some views and Controler redirect.

--- CallCenter/Controllers/AccountController.cs
+++ CallCenter/Controllers/AccountController.cs
@@ -79,7 +79,7 @@ namespace CallCenter.Controllers
             switch (result)
             {
                 case SignInStatus.Success:
-                    return RedirectToLocal(returnUrl);
+                    return RedirectToLocal("/WorkPlatform");
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
