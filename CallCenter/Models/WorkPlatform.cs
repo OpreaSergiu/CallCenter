d0aef67f74d22a16771309672927677e5acabf11
firstbloodidp <sergiu_andrei96@yahoo.com>
2018-03-28 19:54:39 +0300

    Changes

---
+++ CallCenter/Models/WorkPlatform.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CallCenter.Models
+{
+    public class WorkPlatform
+    {
+        public int AccountNumber { get; set; }
+        public string ClientReference { get; set; }
+        public string ClientID { get; set; }
+        public string Name { get; set; }
+        public float AssignAmount { get; set; }
+        public float TotalReceived { get; set; }
+        public float OtherDue { get; set; }
+        public float TotalDue { get; set; }
+        public string Desk { get; set; }
+        public string Status { get; set; }
+        public DateTime PalacementDate { get; set; }
+        public DateTime LastWorkDate { get; set; }
+    }
+}
